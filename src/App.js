422a35b0acbda74698e79979a8d08f5862010b58
Murat Altun <mraltun@gmail.com>
2022-06-27 12:56:54 +0300

    Added category-item and it's scss along with categories array in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
+import CategoryItem from "./components/category-item/category-item";
+
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
   return (
     <div>
       <h1>Hello there</h1>
