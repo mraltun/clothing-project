5a290ae69283064313ed7fe6417f795c46cfdeb1
Murat Altun <mraltun@gmail.com>
2022-06-27 15:18:55 +0300

    Added Navigation route and it's scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Home from "./routes/home/home";
 import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/navigation";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
