47a1dae97a86439cf6d9192c04647ef535fecdb9
Murat Altun <mraltun@gmail.com>
2022-06-30 12:59:49 +0300

    Added logic for adding items to cart. Updated context and empty scss

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../../context/cart";
+
 import Button from "../button/button";
+import CartItem from "../cart-item/cart-item";
 import "./cart-dropdown.scss";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items' />
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} CartItem={item} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
