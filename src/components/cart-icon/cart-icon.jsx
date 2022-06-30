648fb72c587687b7283d1272d60b7df633da89ae
Murat Altun <mraltun@gmail.com>
2022-06-30 12:19:52 +0300

    Updated comments

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/cart";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.scss";
 
 const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  // When user click the Icon set the state opposite (open or close cart-dropdown)
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
   return (
-    <div className='cart-icon-container'>
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
       <span className='item-count'>0</span>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+// Providers
 import { UserProvider } from "./context/user";
 import { ProductsProvider } from "./context/products";
 import { CartProvider } from "./context/cart";
+
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
