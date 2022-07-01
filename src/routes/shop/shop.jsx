db23bb8315cf53059c8af952ed3d77744e4c0de2
Murat Altun <mraltun@gmail.com>
2022-07-01 14:27:05 +0300

    Updated routes for shop and category preview

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,20 +1,12 @@
-import { useContext } from "react";
-import { CategoriesContext } from "../../context/categories";
-import CategoryPreview from "../../components/category-preview/category-preview";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview";
 import "./shop.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className='shop-container'>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 
