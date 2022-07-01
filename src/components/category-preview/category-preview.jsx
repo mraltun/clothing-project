43e3691fba83b8984b4a0501ae3007a327fcb118
Murat Altun <mraltun@gmail.com>
2022-07-01 13:42:25 +0300

    Finishing up category preview

--- src/components/category-preview/category-preview.jsx
+++ src/components/category-preview/category-preview.jsx
@@ -1,7 +1,22 @@
+import ProductCard from "../product-card/product-card";
 import "./category-preview.scss";
 
-const CategoryPreview = () => {
-  return <div>CategoryPreview</div>;
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className='category-preview-container'>
+      <h2>
+        <span>{title.toUpperCase()}</span>
+      </h2>
+      <div className='preview'>
+        {/* Get index of products from the array and list the first 4 of them as preview  */}
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryPreview;
--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,24 +1,20 @@
 import { useContext } from "react";
 import { CategoriesContext } from "../../context/categories";
-import ProductCard from "../../components/product-card/product-card";
+import CategoryPreview from "../../components/category-preview/category-preview";
 import "./shop.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <div key={title}>
-          <h2>{title}</h2>
-          <div className='products-container'>
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      ))}
-    </>
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 
