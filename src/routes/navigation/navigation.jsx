6b46f3d7eac85a8f699065a3ba135eea0f065a53
Murat Altun <mraltun@gmail.com>
2022-07-02 12:06:18 +0300

    Changed navigation router to styled components

--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -7,7 +7,12 @@ import { CartContext } from "../../context/cart";
 import { signOutUser } from "../../utils/firebase/firebase";
 // CRA uses SVGR so we can import SVG as component
 import { ReactComponent as MainLogo } from "../../assets/crown.svg";
-import "./navigation.scss";
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from "./navigation.style";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -15,28 +20,28 @@ const Navigation = () => {
 
   return (
     <>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
+      <NavigationContainer>
+        <LogoContainer to='/'>
           <MainLogo className='logo' />
-        </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
+        </LogoContainer>
+        <NavLinks>
+          <NavLink className='nav-link' to='/shop'>
             SHOP
-          </Link>
+          </NavLink>
           {/* Check state in context if the user logged in then render the correct link  */}
           {currentUser ? (
-            <span className='nav-link' onClick={signOutUser}>
+            <NavLink as='span' className='nav-link' onClick={signOutUser}>
               SIGN OUT
-            </span>
+            </NavLink>
           ) : (
-            <Link className='nav-link' to='/auth'>
+            <NavLink className='nav-link' to='/auth'>
               SING IN
-            </Link>
+            </NavLink>
           )}
           <CartIcon />
-        </div>
+        </NavLinks>
         {isCartOpen && <CartDropdown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </>
   );
